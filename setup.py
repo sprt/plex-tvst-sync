50e438eeaca4910f1d1938acd7c8b02eb18bfc71
sprt <hellosprt@gmail.com>
2015-11-09 21:23:19 +0100

    Add a crontab entry on install

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import ez_setup
 ez_setup.use_setuptools()
 
+from distutils.command.install import install
 from setuptools import setup
+from setuptools import Command
 
-# TODO: setup crontab entry on install
+
+class CustomInstall(install):
+    def _setup_cron(self):
+        import crontab
+        
+        cron = crontab.CronTab(user=True)
+        job = cron.new(command='plex-tvst-sync')
+        job.setall('@hourly')
+        cron.write_to_user(user=True)
+    
+    def run(self):
+        install.run(self)
+        self._setup_cron()
+
 
 setup(
     name='plex-tvst-sync',
@@ -11,8 +26,13 @@ setup(
     author='sprt',
     author_email='hellosprt@gmail.com',
     url='https://github.com/sprt/plex-tvst-sync',
+    cmdclass={'install': CustomInstall},
+    setup_requires=[
+        'python-crontab==1.9.3',
+    ],
     install_requires=[
         'PlexAPI==1.1.0',
+        'python-crontab==1.9.3',
         'requests==2.8.1',
     ],
     entry_points={'console_scripts': ['plex-tvst-sync = scrobbler:main']},
